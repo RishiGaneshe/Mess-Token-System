7ccd35830cf9fa28f53ac800f22b3bf5a965d9db
Abhishek Ganeshe <153019644+RishiGaneshe@users.noreply.github.com>
2025-03-29 01:46:31 +0530

    Add files via upload
    Token Submission Schema Upload

--- models/submittedTokenSchema.js
+++ models/submittedTokenSchema.js
@@ -1,17 +1,15 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const tokenSubmissionSchema = new mongoose.Schema(
-  {
-    token: {  type: mongoose.Schema.Types.ObjectId, ref: "Token", required: true },
-    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    mess_id: { type: String, required: true },
-    createdAt: { type: Date, required: true },
-    expiryDate: { type: Date, required: true }, 
-    submittedAt: { type: Date, default: Date.now }, 
-    amount: { type: Number, required: true },
-    transactionId: { type: String, required: true } 
-  },
-  { timestamps: true }
-);
 
-module.exports = mongoose.model("TokenSubmission", tokenSubmissionSchema);
+const TokenSubmissionSchema = new mongoose.Schema({
+    submissionId: { type: String, unique: true, required: true }, 
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },  
+    username: { type: String, required: true},
+    mess_id: { type: String, required: true },  
+    tokenCount: { type: Number, required: true },  
+    tokenIds: { type: [mongoose.Schema.Types.ObjectId], ref: 'Token', required: true },  
+    submittedAt: { type: Date, default: Date.now },  
+    status: { type: String, enum: ['submitted', 'processed', 'failed'], default: 'submitted' }  
+})
+
+module.exports = mongoose.model('TokenSubmission', TokenSubmissionSchema);
