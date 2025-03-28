7f407f58468d7fca955c7a893b1fb4491d1c9672
Abhishek Ganeshe <153019644+RishiGaneshe@users.noreply.github.com>
2025-03-29 04:12:38 +0530

    Update student.routes.js

--- routes/student.routes.js
+++ routes/student.routes.js
@@ -3,10 +3,11 @@ const router= express.Router()
 const Student= require('../controllers/student.controller')
 
 
-
 router.get("/tokens", Student.handleGetFunctionalTokens)
 
 router.get("/token-data", Student.handleGetTokenData)
+
+router.get("/profile-data", Student.handleGetProfileData)
 
 router.get("/token-history", Student.handleGetTokenHistory)
 
@@ -14,11 +15,17 @@ router.get("/payment-history", Student.handleGetPaymentHistory)
 
 router.get("/submitted-tokens", Student.handleGetTokenSubmissionData)
 
+
+
 router.post("/student-dashboard", Student.handleHelloStudent )
 
 router.post("/token-submission", Student.handlePostTokenSubmission)
 
 router.post("/logout", Student.handlePostStudentLogout)
+
+
+
+router.patch("/profile-update", Student.handlePatchStudentProfile)
 
 
 module.exports= router
