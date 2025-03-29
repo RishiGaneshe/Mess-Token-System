b90d5fa845bdb77f088eaac57f73cae178d78da8
Abhishek Ganeshe <153019644+RishiGaneshe@users.noreply.github.com>
2025-03-29 14:05:19 +0530

    Update emailServices.js

--- services/emailServices.js
+++ services/emailServices.js
@@ -18,3 +18,20 @@ exports.sendSignUpOTP= async (email, otp) =>{
         throw err
     }
 }
+
+
+exports.sendForgetPassOTP= async(email, otp)=>{
+    const msg = {
+        to: email,
+        from: 'info.dakshifoundation@gmail.com',
+        subject: 'OTP for Password Reset',
+        text: `Dear User,\n\nYour One-Time Password (OTP) for Password Reset at MTS is: ${otp}.\n\nPlease do not share this OTP with anyone. It is valid for 2 minutes only.\n\nBest regards,\nMTS`
+    }    
+
+    try{
+        await sgMail.send(msg)
+    }catch(err){
+        console.error("Error in the Sign-up otp sending function "+err.message)
+        throw err
+    }
+}
